db1a4d8c9400aa61f1bbb3596ec7641346399711
Lucas Polizeli <lumpolizeli@gmail.com>
2020-01-30 20:23:40 -0300

    feat: sign in page style
    start to refactor signin component to auth user

--- src/App.js
+++ src/App.js
@@ -2,17 +2,30 @@ import React from 'react';
 import Routes from 'routes';
 import history from 'services/history';
 import { Router } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'assets/scss/paper-dashboard.scss?v=1.1.0';
 import 'assets/demo/demo.css';
 import 'perfect-scrollbar/css/perfect-scrollbar.css';
 
+const theme = {
+  breakpoints: {
+    xs: 0,
+    sm: 576,
+    md: 768,
+    lg: 992,
+    xl: 1200,
+  },
+};
+
 function App() {
   return (
-    <Router history={history}>
-      <Routes />
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router history={history}>
+        <Routes />
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- src/views/SignIn/SignIn.js
+++ src/views/SignIn/SignIn.js
@@ -1,7 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Input, Button } from 'reactstrap';
+import styled from 'styled-components';
+import devcImage from '../../assets/img/logo-devc.png';
+import { breakpoint } from 'styled-components-breakpoint';
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: white;
+
+  ${breakpoint('sm')`
+    background-color: #f4f3ef;
+  `}
+`;
+
+const Logo = styled.img.attrs({
+  src: devcImage,
+})`
+  margin-bottom: 30px;
+`;
+
+const Card = styled.div`
+  width: 400px;
+  height: 300px;
+  display: flex;
+  padding: 15px;
+  border-radius: 5px;
+  flex-direction: column;
+  background-color: white;
+  justify-content: center;
+`;
+
+const InputStyled = styled(Input)`
+  margin-bottom: 10px;
+`;
 
 function SignIn() {
-  return <div>ola</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleOnClickToLogin() {
+    console.log('cliquei');
+  }
+
+  function handleOnChangeEmail({ target: { value: emailToUpdate } }) {
+    setEmail(emailToUpdate);
+  }
+
+  function handleOnChangePassword({ target: { value: passwordToUpdate } }) {
+    setPassword(passwordToUpdate);
+  }
+
+  return (
+    <Container>
+      <Card>
+        <Logo />
+        <InputStyled
+          value={email}
+          type="email"
+          onChange={handleOnChangeEmail}
+          placeholder="Digite seu e-mail"
+        />
+        <InputStyled
+          type="password"
+          value={password}
+          placeholder="Digite sua senha"
+          onChange={handleOnChangePassword}
+        />
+        <Button onClick={handleOnClickToLogin}>Entrar</Button>
+      </Card>
+    </Container>
+  );
 }
 
 export default SignIn;
